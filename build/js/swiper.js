de710ba717374a56dcc7b2c733b67488f39e3414
Arnaldo Muñoz <kings31lendy@gmail.com>
2023-09-15 21:56:09 -0400

    Blog tecnoñogico

---
+++ build/js/swiper.js
@@ -0,0 +1,2 @@
+var swiper2=new Swiper(".testimonials .mySwiper",{slidesPerView:3,loop:!0,pagination:{el:".testimonials .swiper-pagination",clickable:!0}});
+//# sourceMappingURL=swiper.js.map
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Blog Tecnológico
+
+[Ver Diseño](https://www.figma.com/file/yjPEbWWKWpfJFQKxgKGQYJ/Tech-Project?node-id=0%3A1)
